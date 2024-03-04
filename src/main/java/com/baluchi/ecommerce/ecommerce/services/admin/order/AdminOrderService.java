2510c651267bd554165aaa377d1a3638eb99a7bb
hanisadrianaa <its.hanisadriana@gmail.com>
2024-03-04 22:29:21 +0800

    commit AdminOrder

--- src/main/java/com/baluchi/ecommerce/ecommerce/services/admin/order/AdminOrderService.java
+++ src/main/java/com/baluchi/ecommerce/ecommerce/services/admin/order/AdminOrderService.java
@@ -7,4 +7,6 @@ import com.baluchi.ecommerce.ecommerce.dto.OrderDto;
 public interface AdminOrderService {
     
     List<OrderDto> getAllPlacedOrders();
+
+    OrderDto changeOrderStatus(Long orderId, String status);
 }
--- src/main/java/com/baluchi/ecommerce/ecommerce/services/admin/order/AdminOrderServiceImpl.java
+++ src/main/java/com/baluchi/ecommerce/ecommerce/services/admin/order/AdminOrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.baluchi.ecommerce.ecommerce.services.admin.order;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -23,5 +25,21 @@ public class AdminOrderServiceImpl implements AdminOrderService {
         List<Order> orderList = orderRepository.findAllByOrderStatusIn(List.of(OrderStatus.Placed, OrderStatus.Shipped, OrderStatus.Delivered));
 
         return orderList.stream().map(Order::getOrderDto).collect(Collectors.toList());
+    }
+
+    public OrderDto changeOrderStatus(Long orderId, String status) {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+        if (optionalOrder.isPresent()) {
+            Order order = optionalOrder.get();
+
+            if (Objects.equals(status, "Shipped")) {
+                order.setOrderStatus(OrderStatus.Shipped);
+            } else if (Objects.equals(status, "Delivered")) {
+                order.setOrderStatus(OrderStatus.Delivered);
+            }
+            return orderRepository.save(order).getOrderDto();
+        }
+
+        return null;
     }
 }
