0651b22f646a41d63ad34baa1bb1499413cdfccb
hanisadrianaa <its.hanisadriana@gmail.com>
2024-03-05 02:36:42 +0800

    Update ReviewDto

--- src/main/java/com/baluchi/ecommerce/ecommerce/dto/ReviewDto.java
+++ src/main/java/com/baluchi/ecommerce/ecommerce/dto/ReviewDto.java
@@ -1,17 +1,7 @@
 package com.baluchi.ecommerce.ecommerce.dto;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.baluchi.ecommerce.ecommerce.entity.Product;
-import com.baluchi.ecommerce.ecommerce.entity.User;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
