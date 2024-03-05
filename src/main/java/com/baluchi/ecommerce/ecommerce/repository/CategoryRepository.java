2335962d8bde9c3853c612ce7d7114e0551ea604
hanisadrianaa <its.hanisadriana@gmail.com>
2024-03-05 14:32:15 +0800

    Update CategoryRepository

--- src/main/java/com/baluchi/ecommerce/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/baluchi/ecommerce/ecommerce/repository/CategoryRepository.java
@@ -1,5 +1,8 @@
 package com.baluchi.ecommerce.ecommerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,9 @@ import com.baluchi.ecommerce.ecommerce.entity.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     
-    
+    List<Category> findAll();
+
+    Optional<Category> findById(Long id);
+
+    Optional<Category> findByName(String name);
 }
